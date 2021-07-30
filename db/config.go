ef65a6bb08c0e8f79ec758d5e532f201e6258a70
Juan Farber <jfarber@onebittech.com>
2021-07-30 11:46:37 -0300

    Agrego topologia y crear switch

--- db/config.go
+++ db/config.go
@@ -1,9 +1,6 @@
 package db
 
-<<<<<<< HEAD
 const DB_HOST = "mongodb+srv://juantuc98:juantuc98@db-wimp.yeslm.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
-=======
->>>>>>> c8b6e1c787285e1c4daa734f3119314019a276e5
 const DB_NOMBRE = "wimp"
 const COL_USUARIOS = "usuarios"
 const COL_SWITCHES = "switches"
